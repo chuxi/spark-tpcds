e3ad567f0bf01fb7648a8a19c91bb62d80354df9
Hanwei <chuxiking@163.com>
2017-11-13 20:15:03 +0800

    windjammer tpcds script

--- src/main/scala/cn/edu/zju/king/Setup.scala
+++ src/main/scala/cn/edu/zju/king/Setup.scala
@@ -1,7 +1,10 @@
 package cn.edu.zju.king
 
+import java.io.{File, FileOutputStream}
+
 import com.databricks.spark.sql.perf.tpcds.{TPCDS, TPCDSTables}
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 
 
 object Setup {
@@ -21,7 +24,7 @@ object Setup {
     val location = "/tmp/tpcds/data"
     val databaseName = "benchmark"
     val scaleFactor = "1"
-    val format = "parquet"
+    val format = "windjammer"
 
     val sqlContext = spark.sqlContext
 
@@ -29,17 +32,37 @@ object Setup {
 
     tables.genData(location, format, overwrite = true, partitionTables = false, clusterByPartitionColumns = true, filterOutNullPartitionValues = false, tableFilter = "", numPartitions = 32)
 
-//    tables.createExternalTables(location, format, databaseName, overwrite = true, discoverPartitions = true)
+    tables.createExternalTables(location, format, databaseName, overwrite = true, discoverPartitions = false)
 
-    tables.createTemporaryTables(location, format)
-
+    // query
     val iterations = 1
     val timeout = 24 * 60 * 60
 
+    spark.sql(s"use $databaseName")
+
     val tpcds = new TPCDS(spark.sqlContext)
 
-    val experiment = tpcds.runExperiment(tpcds.tpcds1_4Queries, iterations = iterations)
+    val experiment = tpcds.runExperiment(tpcds.targetedPerfQueries, iterations = iterations)
     experiment.waitForFinish(timeout)
+
+    val summary = experiment.getCurrentResults()
+      .withColumn("Name", substring(col("name"), 2, 100))
+      .withColumn("Runtime", (col("parsingTime") + col("analysisTime") + col("optimizationTime") + col("planningTime") + col("executionTime")) / 1000.0)
+      .select("Name", "Runtime", "tables", "failure")
+
+    // write to local file
+    import java.io.PrintWriter
+    val writer = new PrintWriter(s"$location/currentResult.txt", "UTF-8")
+    summary.collect().foreach { row =>
+      row.toSeq.foreach { cell =>
+        writer.print(cell)
+        writer.print("\t")
+      }
+      writer.println()
+    }
+    writer.close()
+
+    summary.show(numRows = 1000)
 
     spark.stop()
   }
